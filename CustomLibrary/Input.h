d13e9e292fa294dbcab777b748cda0f4bbbb8729
Michael Bloch <mchblch@gmail.com>
2018-07-25 16:34:41 +0200

    Unit Conversion complete & Input changed

--- CustomLibrary/Input.h
+++ CustomLibrary/Input.h
@@ -3,6 +3,7 @@
 
 #include <limits>
 #include <ios>
+#include <string>
 
 template<typename T>
 constexpr const T getConsoleInput()
@@ -15,7 +16,7 @@ constexpr const T getConsoleInput()
 		{
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			std::cout << "Invalid." << std::endl;
+			std::cerr << "Invalid." << std::endl;
 		}
 		else
 		{
@@ -23,6 +24,39 @@ constexpr const T getConsoleInput()
 			return input;
 		}
 	}
+}
+
+const std::wstring getStringInput(const std::wstring *list, const short &length)
+{
+	std::wstring type;
+	while (true)
+	{
+		std::getline(std::wcin, type);
+		if (type == L"help")
+			for (short i = 0; i < length; i++)
+				std::wcout << "\"" << list[i] << "\"\n";
+		else if ([&list, &length, &type]() { for (short i = 0; i < length; i++) if (list[i] == type) return true; return false; }())
+			return type;
+		else
+			std::wcerr << L"Invalid." << std::endl;
+	}
+	return type;
+}
+const std::string getStringInput(const std::string *list, const short &length)
+{
+	std::string type;
+	while (true)
+	{
+		std::getline(std::cin, type);
+		if (type == "help")
+			for (short i = 0; i < length; i++)
+				std::cout << "\"" << list[i] << "\"\n";
+		else if ([&list, &length, &type]() { for (short i = 0; i < length; i++) if (list[i] == type) return true; return false; }())
+			return type;
+		else
+			std::cerr << "Invalid." << std::endl;
+	}
+	return type;
 }
 
 #endif // !CUSTOMINPUT
