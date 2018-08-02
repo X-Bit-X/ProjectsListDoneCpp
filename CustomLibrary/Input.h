5851e37b22101fdd466a311024b9c3793bc1ce80
Michael Bloch <mchblch@gmail.com>
2018-08-02 10:28:10 +0200

    Complex Numbers completed & improved Input

--- CustomLibrary/Input.h
+++ CustomLibrary/Input.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <type_traits>
+#include <vector>
 
 template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
 constexpr const T getConsoleInput()
@@ -27,6 +28,7 @@ constexpr const T getConsoleInput()
 	}
 }
 
+//They stay because of old code
 const std::wstring getStringInput(const std::wstring *list, const short &length)
 {
 	std::wstring type;
@@ -56,6 +58,40 @@ const std::string getStringInput(const std::string *list, const short &length)
 			return type;
 		else
 			std::cerr << "Invalid." << std::endl;
+	}
+	return type;
+}
+
+//New improved
+const std::string getStringInput(const std::vector<std::string> &list)
+{
+	std::string type;
+	while (true)
+	{
+		std::getline(std::cin, type);
+		if (type == "help")
+			for (short i = 0; i < list.size(); i++)
+				std::cout << "\"" << list[i] << "\"\n";
+		else if ([&list, &type]() { for (const auto &val : list) if (val == type) return true; return false; }())
+			return type;
+		else
+			std::cerr << "Invalid." << std::endl;
+	}
+	return type;
+}
+const std::wstring getStringInput(const std::vector<std::wstring> &list)
+{
+	std::wstring type;
+	while (true)
+	{
+		std::getline(std::wcin, type);
+		if (type == L"help")
+			for (short i = 0; i < list.size(); i++)
+				std::wcout << L"\"" << list[i] << L"\"\n";
+		else if ([&list, &type]() { for (const auto &val : list) if (val == type) return true; return false; }())
+			return type;
+		else
+			std::wcerr << L"Invalid." << std::endl;
 	}
 	return type;
 }
